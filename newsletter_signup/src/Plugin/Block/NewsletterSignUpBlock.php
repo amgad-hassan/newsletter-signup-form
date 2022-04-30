38ade27bb413b17f4c776532ee594660d327851c
AMGADHASSAN <amgadhassan28@gmail.com>
2022-04-30 16:43:12 +0200

    Create the block of the sign-up form

---
+++ newsletter_signup/src/Plugin/Block/NewsletterSignUpBlock.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\newsletter_signup\Plugin\Block\NewsletterSignUpBlock.
+ */
+
+namespace Drupal\newsletter_signup\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+
+/**
+ * Provides a 'Newsletter Sign-up Form' Block.
+ *
+ * @Block(
+ *   id = "newsletter_signup_form_block",
+ *   admin_label = @Translation("Newsletter Sign-up Form Block"),
+ *   category = @Translation("Newsletter Sign-up"),
+ * )
+ */
+class NewsletterSignUpBlock extends BlockBase
+{
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build()
+  {
+    $form = \Drupal::formBuilder()->getForm('\Drupal\newsletter_signup\Form\SignupForm');
+
+    return $form;
+    
+  }
+}
